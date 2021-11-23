8f769fb73feee66e5604526967b2b544b505e712
from97 <skm1104@daum.net>
2021-11-24 01:14:49 +0900

    add class name to tags

--- src/components/optionBar/FilterSelector.tsx
+++ src/components/optionBar/FilterSelector.tsx
@@ -6,21 +6,20 @@ interface IPros {}
 const FilterSelector: React.FC<IPros> = () => {
   return (
     <div className="ac-datagrid--option_bar__filter_selector">
-      <ul>
-        <li>
+      <ul className="filter_selector__list">
+        <li className="filter_selector__item">
           <label>Column</label>
           <FilterDropdown />
         </li>
-        <li>
+        <li className="filter_selector__item">
           <label>Value</label>
           <input type="text" />
         </li>
-        <li>
+        <li className="filter_selector__item">
           <label>Operators</label>
           <FilterDropdown />
         </li>
       </ul>
-      <button type="button">+</button>
     </div>
   );
 };
