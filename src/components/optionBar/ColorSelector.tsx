e64d8ecf923f76e731688a3092496fb5b83e814a
hotkimho <rlaghekd777@naver.com>
2021-11-26 02:05:14 +0900

    fix : ColorSelector__style__color sharing error

--- src/components/optionBar/ColorSelector.tsx
+++ src/components/optionBar/ColorSelector.tsx
@@ -1,21 +1,31 @@
 import * as React from "react";
 import { ThemeContextActionTypes } from "../../@interface";
-import { useDatagridThemeDispatch } from "../../context/DatagridThemeContext";
+import { useDatagridThemeContext,
+  useDatagridThemeDispatch
+} from "../../context/DatagridThemeContext";
 import { presetItems } from "./themeItems/ThemePreset";
+import { themeProperties } from "./themeItems/ThemeProperties";
 interface IPros {
   onMouseLeave: () => void;
 }
 
 const ColorSelector: React.FC<IPros> = (props) => {
   const { onMouseLeave } = props;
+  const colorContext = useDatagridThemeContext();
   const colorDispatch = useDatagridThemeDispatch();
 
   const onClickHandler: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    const themeStyle = document.querySelector(`.${colorContext._theme}`) as HTMLParagraphElement;
+
+    themeProperties.map((prop) => (
+      themeStyle.style.removeProperty(prop)
+    ))
     colorDispatch({
       type: ThemeContextActionTypes.SET_THEME,
       theme: e.currentTarget.innerText,
     });
   };
+
   return (
     <div className="ac-datagrid--option_bar__color_selector" onMouseLeave={onMouseLeave}>
       <ul className="color_selector__list">
