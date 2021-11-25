0786c68450f8e9bae59dc6f47b0ed0f486138a99
유병우 <yubyeong-u@yubyeong-uui-MacBookPro.local>
2021-11-25 13:23:08 +0900

    pull before commit

--- src/components/body/BodyTableRow.tsx
+++ src/components/body/BodyTableRow.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { useDatagridContext } from "../../context/DatagridContext";
-import { IColumn, IDataItem } from "../../@interface";
+import { useDatagridContext, useDatagridDispatch } from "../../context/DatagridContext";
+import { IColumn, IDatagridContext, IDataItem } from "../../@interface";
 
 interface IProps {
   columns: IColumn[];
@@ -15,7 +15,9 @@ type onPos = {
 
 const BodyTableRow: React.FC<IProps> = ({ columns, rowIndex, rowItem }) => {
   const context = useDatagridContext();
+  const dispatch = useDatagridDispatch();
   const [onEdit, setOnEdit] = React.useState<onPos>({col: -1, row: -1});
+  const [editingText, setEditingText] = React.useState("");
 
   const containerStyle = React.useMemo(
     () => ({
@@ -28,12 +30,15 @@ const BodyTableRow: React.FC<IProps> = ({ columns, rowIndex, rowItem }) => {
 
   const customClickHandler: React.MouseEventHandler<HTMLTableDataCellElement> = (evt) => {
     evt.preventDefault();
+    console.log(context.data)
     const value = evt.currentTarget.dataset.value;
     const colIdx = evt.currentTarget.dataset.col
     const rowIdx = rowIndex;
-    onClick?.();
-    if(colIdx)
-      onEditing(evt, Number.parseInt(colIdx), rowIdx);
+    console.log(context.data);
+    if(colIdx === undefined)
+      return;
+    onClick?.(evt, value, rowIdx,  Number.parseInt(colIdx));
+    onEditing(evt, Number.parseInt(colIdx), rowIdx);
   }
 
   const onEditing = (evt : React.MouseEvent, colIdx: number, rowIdx: number) => {
@@ -41,6 +46,18 @@ const BodyTableRow: React.FC<IProps> = ({ columns, rowIndex, rowItem }) => {
   };
 
   const onBlur:React.FocusEventHandler<HTMLInputElement> = (evt) => {
+    const colIdx: number = Number.parseInt(evt.currentTarget.id);
+    const nextState: IDatagridContext = {
+        ...context
+    };
+    const nextData:any = nextState.data;
+    const key= context._colGroup?.[colIdx].key;
+    if(key !== undefined) {
+      // columns와 데이터 불일치로 인해 문제 발생
+      nextData[rowIndex]["value"][key] = evt.currentTarget.value;
+      console.log(nextData[rowIndex]["value"][key]);
+    }
+    // 디스패처 사용 안했는데 데이터 변경이 발생
     setOnEdit({...onEdit, col: -1, row: -1});
   }
 
@@ -51,14 +68,12 @@ const BodyTableRow: React.FC<IProps> = ({ columns, rowIndex, rowItem }) => {
         : rowItem.value[String(col.key)];
       return (
         <td key={ci}  onClick={customClickHandler} data-col ={ci} data-value ={item}>
-          { ci === onEdit.col && rowIndex == onEdit.row ?  <input type="text" onBlur = {onBlur} autoFocus={true}/> : <span>{item}</span>}
+          { ci === onEdit.col && rowIndex == onEdit.row ?  <input id={ci.toString()} type="text" onBlur = {onBlur} autoFocus={true} style={{width:90}}/> : <span>{item}</span>}
         </td>
       );
     },
     [rowItem.value, onEdit]
   );
-
-
 
   return (
     <tr style={containerStyle}>
