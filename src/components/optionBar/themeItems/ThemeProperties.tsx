92f96183af921a2236850fab27453cb8c6898982
kimho <rlaghekd777@naver.com>
2021-11-25 21:27:05 +0900

    add : ThemeProperties__theme variables__add theme variables

---
+++ src/components/optionBar/themeItems/ThemeProperties.tsx
@@ -0,0 +1,25 @@
+const themeProperties = [
+    "--bg",
+    "--header-bg",
+    "--tr-bg",
+    "--line-number-bg",
+    "--scroll-bar-color",
+    "--status-bg",
+    "--status-scroll-bar-bg",
+    "--txt-header-color",
+    "--txt--default-color",
+]
+
+const themeLabel = [
+    "bg",
+    "header-bg",
+    "tr-bg",
+    "line-number-bg",
+    "scroll-bar-bg",
+    "status-bg",
+    "status-scroll-bar-bg",
+    "txt-header-color",
+    "txt-default-color",
+]
+
+export { themeProperties, themeLabel };
