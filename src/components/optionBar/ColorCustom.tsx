a942a22902b6021d022e6b644d229778d2e8877c
kimho <rlaghekd777@naver.com>
2021-11-26 00:13:55 +0900

    add : ColorCustom__palette__add palatte and label

--- src/components/optionBar/ColorCustom.tsx
+++ src/components/optionBar/ColorCustom.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { ThemeContextActionTypes } from "../../@interface";
+import { useDatagridThemeContext } from "../../context/DatagridThemeContext";
+import { themeProperties, themeLabel } from "./themeItems/ThemeProperties";
 
 interface IPros {
   onMouseLeave: () => void;
@@ -7,12 +9,29 @@ interface IPros {
 
 const ColorCustom: React.FC<IPros> = (props) => {
   const { onMouseLeave } = props;
+  const themeContext = useDatagridThemeContext();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const themeStyle = document.querySelector(`.${themeContext._theme}`) as HTMLParagraphElement;
+    themeStyle.style.setProperty(e.target.name, e.target.value);
+  }
 
   return (
     <div className="ac-datagrid--option_bar__custom_selector" onMouseLeave={onMouseLeave}>
       <ul className="color_selector__list">
         <li className="color_selector__item">
-          lulu
+          {
+            (themeProperties || []).map((prop, index) => (
+              <div>
+                <label>{themeLabel[index]}</label>
+                <input type="color"
+                  onChange={onChange}
+                  name={prop}
+                  key={index}
+                />
+              </div>
+            ))
+          }
         </li>
       </ul>
     </div>
