bf05574f61b87a5cfe2509e7e6a98a2902586637
kimho <rlaghekd777@naver.com>
2021-11-25 18:28:54 +0900

    add Palette

--- src/components/optionBar/OptionBar.tsx
+++ src/components/optionBar/OptionBar.tsx
@@ -4,16 +4,36 @@ import FilterToggle from "./FilterToggle";
 import FilterSelector from "./FilterSelector";
 import ColorToggle from "./ColorToggle";
 import ColorSelector from "./ColorSelector";
+import { ThemeContextActionTypes } from "../../@interface";
+import { useDatagridThemeContext } from "../../context/DatagridThemeContext";
 
 const OptionBar: React.FC<IDatagridOptionBar> = () => {
   const [display, setDisplay] = React.useState(false);
+  const themeContext = useDatagridThemeContext();
+  const themeColors = [
+    "--bg",
+    "--header-bg",
+    "--tr-bg",
+    "--line-number-bg",
+    "--scroll-bar-color",
+    "--status-bg",
+    "--status-scroll-bar-bg",
+    "--txt-header-color",
+    "--txt--default-color",
+  ];
 
   const onClick = () => {
     setDisplay((prev) => !prev);
   };
 
+  const onPaletteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const themeStyle = document.querySelector(`.${themeContext._theme}`) as HTMLParagraphElement;
+    themeStyle.style.setProperty(e.target.name, e.target.value);
+  }
+
   return (
     <div className="ac-datagrid--option_bar">
+
       <ColorToggle onClick={onClick} />
       {display && <ColorSelector />}
     </div>
