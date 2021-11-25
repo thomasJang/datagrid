bffefadbd800b266b0addcde69adba0849d6d9fa
kimho <rlaghekd777@naver.com>
2021-11-25 21:52:01 +0900

    fix : ColorCustom__style__fix style

--- src/components/optionBar/ColorCustom.tsx
+++ src/components/optionBar/ColorCustom.tsx
@@ -3,13 +3,13 @@ import { ThemeContextActionTypes } from "../../@interface";
 
 interface IPros {
   onMouseLeave: () => void;
- }
+}
 
 const ColorCustom: React.FC<IPros> = (props) => {
-  const {onMouseLeave} = props;
+  const { onMouseLeave } = props;
 
   return (
-    <div className="ac-datagrid--option_bar__color_selector ac-datagrid--option_bar__custom_selector" onMouseLeave={onMouseLeave}>
+    <div className="ac-datagrid--option_bar__custom_selector" onMouseLeave={onMouseLeave}>
       <ul className="color_selector__list">
         <li className="color_selector__item">
           lulu
